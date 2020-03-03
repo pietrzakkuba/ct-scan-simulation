9021a3cece2af22e150315a0f54df9acca5b5d24
pietrzakkuba <pietrzakkuba@yahoo.pl>
2020-03-03 20:42:08 +0100

    image load

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print('Hello world')
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import numpy as np
+import pydicom
+from bresenham import bresenham
+
+img = mpimg.imread('shepp-logan.png')
+plt.imshow(img, cmap='gray')
+print(img.shape)
+plt.show()
